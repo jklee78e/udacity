ff47a0a951063c0215a7c74fc7e580f019a4ab82
jklee78e <jklee78e@gmail.com>
2019-03-02 06:09:16 -0800

    third

--- ball_chaser/src/drive_bot.cpp
+++ ball_chaser/src/drive_bot.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 //TODO: Include the ball_chaser "DriveToTarget" header file
+#include "ball_chaser/DriveToTarget.h"
 #include <std_msgs/Float64.h>
 
 // ROS::Publisher motor commands;
@@ -9,20 +10,20 @@ ros::Publisher motor_command_publisher;
 // TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
 // This function should publish the requested linear x and angular velocities to the robot wheel joints
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
-bool handle_drive_request(ball_chase::DriveToTarget::Request& req, ball_chase::DriveToTarget::Request& res)
+bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
 {
    // TODO: Delete the loop, move the code to the inside of the callback function and make the necessary changes to publish the requested velocities instead of constant values
-   ROS_INFO("GoToPositionRequest received - j1:%1.2f, j2:%1.2f", (float)req.linear.x, (float)req.angular.z);
+   ROS_INFO("GoToPositionRequest received - j1:%1.2f, j2:%1.2f", (float)req.linear_x, (float)req.angular_z);
 
    // Create a motor_command object of type geometry_msgs::Twist
    geometry_msgs::Twist motor_command;
    // Set wheel velocities, forward [0.5, 0.0]
-   motor_command.linear.x = 0.5;
-   motor_command.angular.z = 0.0;
+   motor_command.linear.x = req.linear_x;
+   motor_command.angular.z = req.angular_z;
    // Publish angles to drive the robot
    motor_command_publisher.publish(motor_command);
 
-    res.msg_feedback = "Linear Spd x:"+std::to_string(motor_command.linear.x)+", Angular Spd z : "+std::to_string(motor_command.angular.z);
+   res.msg_feedback = "Linear Spd x:"+std::to_string(motor_command.linear.x)+", Angular Spd z : "+std::to_string(motor_command.angular.z);
   
  }
 
